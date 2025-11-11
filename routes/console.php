e4e8db99a18887531f67e5614b228da29c8b4c39
Chetan Moradiya <chetan.tatvamasi@gmail.com>
2025-11-11 18:28:27 +0530

    fault set

--- routes/console.php
+++ routes/console.php
@@ -4,7 +4,7 @@ use Illuminate\Foundation\Inspiring;
 use Illuminate\Support\Facades\Artisan;
 use Illuminate\Support\Facades\Schedule;
 
-// Artisan::command('inspire', function () {
+ // Artisan::command('inspire', function () {
 //     $this->comment(Inspiring::quote());
 // })->purpose('Display an inspiring quote');
 //Schedule::command('inverterWeeklyGeneration:cron')->weeklyOn(1, '4:15')->runInBackground()->withoutOverlapping();
