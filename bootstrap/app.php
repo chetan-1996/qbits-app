fa3745bd192d714e24617ddfd45697cdc1f62ed7
Chetan Moradiya <chetan.tatvamasi@gmail.com>
2025-11-13 10:49:50 +0530

    csrf cookies

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -13,6 +13,12 @@ return Application::configure(basePath: dirname(__DIR__))
     )
     ->withMiddleware(function (Middleware $middleware): void {
         $middleware->statefulApi();
+       // Define the API middleware group
+        $middleware->group('api', [
+            \Laravel\Sanctum\Http\Middleware\EnsureFrontendRequestsAreStateful::class,
+           // 'throttle:api',
+            \Illuminate\Routing\Middleware\SubstituteBindings::class,
+        ]);
     })
     ->withExceptions(function (Exceptions $exceptions): void {
         //
