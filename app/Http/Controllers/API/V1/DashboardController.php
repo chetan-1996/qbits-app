35b8307a96a9086677d940ac0d482d2be191bc7e
Chetan Moradiya <chetan.tatvamasi@gmail.com>
2026-01-29 14:57:11 +0530

    widget count

--- app/Http/Controllers/API/V1/DashboardController.php
+++ app/Http/Controllers/API/V1/DashboardController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers\API\V1;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 
 class DashboardController extends BaseController
 {
