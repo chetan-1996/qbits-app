82a63257b8543f4bf158aeded470e93fd459c984
Chetan Moradiya <chetan.tatvamasi@gmail.com>
2026-01-30 14:59:32 +0530

    add dashbaord api in frontend side

--- app/Http/Controllers/API/V1/DashboardController.php
+++ app/Http/Controllers/API/V1/DashboardController.php
@@ -5,7 +5,7 @@ namespace App\Http\Controllers\API\V1;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
-
+use Illuminate\Support\Facades\Auth;
 
 class DashboardController extends BaseController
 {
