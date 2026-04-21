e4f248a9a0ec522cd8462daef0e1796b3acf4c92
Chetan Moradiya <chetan.tatvamasi@gmail.com>
2026-04-21 14:36:41 +0530

    Ongridrooftop data pub command

--- app/Console/Commands/MqttTelemetryListener.php
+++ app/Console/Commands/MqttTelemetryListener.php
@@ -17,18 +17,32 @@ class MqttTelemetryListener extends Command
         $mqtt = new MqttService();
         $mqtt->connect(config('mqtt.client_id_prefix') .'-telemetry-sub');
 
-        $mqtt->subscribe('heaven/devices/+/telemetry', function ($topic, $message) {
+        $mqtt->subscribe('rtsg-1/Ongridrooftop/+/data/pub', function ($topic, $message) {
 
             $payload = json_decode($message, true);
 
             DB::table('telemetry_raw')->insert([
-                'collector_id' => $payload['cid'] ?? null,
-                'inverter_id'  => $payload['inv_id'] ?? null,
+                'collector_id' => $payload['IMEI'] ?? null,
+                'inverter_id'  => $payload['IMEI'] ?? null,
                 'payload'      => $message,
                 'created_at'   => now(),
             ]);
 
             $this->info("TELEMETRY: $topic");
         });
+
+        // $mqtt->subscribe('heaven/devices/+/telemetry', function ($topic, $message) {
+
+        //     $payload = json_decode($message, true);
+
+        //     DB::table('telemetry_raw')->insert([
+        //         'collector_id' => $payload['cid'] ?? null,
+        //         'inverter_id'  => $payload['inv_id'] ?? null,
+        //         'payload'      => $message,
+        //         'created_at'   => now(),
+        //     ]);
+
+        //     $this->info("TELEMETRY: $topic");
+        // });
     }
 }
