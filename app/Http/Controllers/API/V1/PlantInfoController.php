49078b0e61558681f7d6767d149f20efb6cb66a2
Chetan Moradiya <chetan.tatvamasi@gmail.com>
2026-08-29 10:44:05 +0530

    comment out company-level filtering in frontendIndex endpoint and revert to single user_id filtering instead of checking user_flag and qbits_company_code

--- app/Http/Controllers/API/V1/PlantInfoController.php
+++ app/Http/Controllers/API/V1/PlantInfoController.php
@@ -441,14 +441,14 @@ class PlantInfoController extends BaseController
         $cursorKey = "pi_cursor_map:{$id}:p{$page}";
         $cursor = Cache::get($cursorKey, 0);
 
-        $user = Auth::user();
+        // $user = Auth::user();
 
-        $companyId = [$id];
-        if ($user->user_flag == 1 && !is_null($user->qbits_company_code) && $user->qbits_company_code !== '') {
-            $companyId = Client::where('qbits_company_code', $user->qbits_company_code)
-                ->pluck('id')
-                ->all();
-        }
+        // $companyId = [$id];
+        // if ($user->user_flag == 1 && !is_null($user->qbits_company_code) && $user->qbits_company_code !== '') {
+        //     $companyId = Client::where('qbits_company_code', $user->qbits_company_code)
+        //         ->pluck('id')
+        //         ->all();
+        // }
 
         // 2. FETCH DATA FOR THIS PAGE (KEYSET)
         $query = DB::table('plant_infos')
@@ -460,7 +460,7 @@ class PlantInfoController extends BaseController
                 'admin_phone','installer_phone','no_of_panel',
                 'panel_watt_peak','watchlist_flag'
             ])
-            ->whereIn('user_id', $companyId)
+            ->where('user_id', $id)
             ->when($search, fn($q) => $q->where('plant_name', 'like', "%{$search}%"))
             ->when($watchlistFlag !== null, fn($q) => $q->where('watchlist_flag', $watchlistFlag))
             ->when($cursor > 0, fn($q) => $q->where('id', '>', $cursor))
